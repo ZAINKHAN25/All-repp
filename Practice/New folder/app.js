cb8383e2ecbdb7250d4d12a0ae76c675017f2516
ZAINKHAN25 <zainarfeen87@gmail.com>
2023-02-24 12:14:41 +0500

    12:14 // 2/24/2023

---
+++ Practice/New folder/app.js
@@ -0,0 +1,20 @@
+// var names = {
+//     first: "Zain",
+//     second: "Huzaifa",
+//     third: "don't know",
+//     cars:["Lamborghini", "Swift", "Rolls Royce"]
+// };
+// console.log(names.cars)
+
+// console.log(names.cars.push("Ferari"))
+// console.log(names.cars)
+
+// console.log(names.cars.push("Mehran"))
+// console.log(names.cars)
+
+// let name = 1
+let number = +prompt("Write your name here")
+
+for (let i = 1; i <= 10000; i++) {
+    console.log(` ${number} X ${i} = ${number * i}<br />`)   
+}
