8ae0123927244d89cfd6ae3e7dafa0a35a3f8083
ZAIN-KHAN-25 <121414309+ZAINKHAN25@users.noreply.github.com>
2023-02-24 09:32:03 +0500

    9:31 // 2/24/ 2023

---
+++ Java Script/app.js
@@ -0,0 +1,20 @@
+var names = {
+    first: "Zain",
+    second: "Huzaifa",
+    third: "don't know",
+    cars:["Lamborghini", "Swift", "Rolls Royce"]
+};
+console.log(names.cars)
+
+console.log(names.cars.push("Ferari"))
+console.log(names.cars)
+
+console.log(names.cars.push("Mehran"))
+console.log(names.cars)
+
+let name = 1
+let number = +prompt("Write your name here")
+
+// for (let i = 1; i <= 10; i++) {
+//     document.write(`<table border="1"> <tr> <td>${number}</td> <td>X</td> <td>${i} </td><td>=</td> <td>${number * i}</td><br /></tr></table>`)   
+// }
